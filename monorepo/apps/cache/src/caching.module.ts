391b612a2f06a022e9bf008e1ffca2d6ff7315cc
Nam Nguyen <namnh240795@gmail.com>
2024-06-05 16:51:56 +0700

    feat: fix caching module

--- monorepo/apps/cache/src/caching.module.ts
+++ monorepo/apps/cache/src/caching.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CachingController } from './caching.controller';
 import { CachingService } from './caching.service';
+import { HealthcheckModule } from '@namnh240795/healthcheck';
 
 @Module({
-  imports: [],
+  imports: [HealthcheckModule],
   controllers: [CachingController],
   providers: [CachingService],
 })
--- monorepo/apps/cache/src/main.ts
+++ monorepo/apps/cache/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { CachingModule } from './caching.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(CachingModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cache API')
+    .setDescription('The Cache API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
