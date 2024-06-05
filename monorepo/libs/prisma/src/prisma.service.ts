385049dbef786c8e20f4b86179449eb276f1be69
Nam Nguyen <namnh240795@gmail.com>
2024-06-05 12:01:42 +0700

    feat: add prisma as library

---
+++ monorepo/libs/prisma/src/prisma.service.ts
@@ -0,0 +1,11 @@
+import { OnModuleInit } from '@nestjs/common';
+import { Global, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Global()
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
