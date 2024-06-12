02f28b5e9c9bcdff2fa88d5b0f9b4aca63949d34
Nam Nguyen <namnh240795@gmail.com>
2024-06-12 14:21:16 +0700

    feat: update healthcheck message

--- monorepo/libs/healthcheck/src/healthcheck.controller.ts
+++ monorepo/libs/healthcheck/src/healthcheck.controller.ts
@@ -3,11 +3,11 @@ import { ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { HealthcheckService } from './healthcheck.service';
 
 class HealthcheckRes {
-  @ApiProperty()
+  @ApiProperty({ description: 'Healthcheck message' })
   message: string;
 }
 
-@ApiTags('/healthcheck')
+@ApiTags('health-check')
 @Controller('healthcheck')
 export class healthcheckController {
   constructor(private readonly healthCheckService: HealthcheckService) {}
